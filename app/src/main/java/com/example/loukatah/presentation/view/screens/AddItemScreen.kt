b54379a2bdcb6b900fcbe256f2c13a53cbd166a9
Oussama <khelefoussama2005@gmail.com>
2025-03-11 22:12:02 +0100

    TP3 done

--- app/src/main/java/com/example/loukatah/presentation/view/screens/AddItemScreen.kt
+++ app/src/main/java/com/example/loukatah/presentation/view/screens/AddItemScreen.kt
@@ -31,8 +31,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.BeyondBoundsLayout
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.loukatah.presentation.viewmodel.AddItemEvent
 import com.example.loukatah.presentation.viewmodel.AddItemViewModel
 import com.example.loukatah.presentation.viewmodel.ItemCategoryViewModel
 
@@ -47,13 +53,33 @@ fun AddItemScreen(
     addItemViewModel: AddItemViewModel = hiltViewModel()
 ) {
     // State
-    
+    //--status
+    var statusIsExpanded by remember { mutableStateOf(false) };
+    var status by remember { mutableStateOf("Lost") }
+    //--title
+    var title by remember { mutableStateOf("") }
+    //--description
+    var description by remember { mutableStateOf("") }
+    //--image url
+    var imageUrl by remember { mutableStateOf("") }
+    //--category
+    var categoryIsExpanded by remember { mutableStateOf(false) };
+    var categoryValue by remember { mutableStateOf("") }
     val categoryState by itemCategoryViewModel.categoryState.collectAsState()
     val categories = categoryState.categories
-
+    //--uistate
+    val uiState by addItemViewModel.uiState.collectAsState()
     // Events
-
-    
+    //--title
+    addItemViewModel.onEvent(AddItemEvent.TitleChange(title))
+    //--description
+    addItemViewModel.onEvent(AddItemEvent.DescriptionChange(description))
+    //--category
+    addItemViewModel.onEvent(AddItemEvent.CategoryChange(categoryValue))
+    //--status
+    addItemViewModel.onEvent(AddItemEvent.StatusChange(status))
+    //--image url
+    addItemViewModel.onEvent(AddItemEvent.PictureChange(imageUrl))
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
@@ -83,52 +109,68 @@ fun AddItemScreen(
             ) {
                 // Title
                 OutlinedTextField(
-                    value = "",//TODO() ,
-                    onValueChange = { },//TODO() ,
+                    value = title,//TODO() ,
+                    onValueChange = { title = it},//TODO() ,
                     label = { Text("Title") },
+                    textStyle = TextStyle(textAlign = TextAlign.Right),
                     modifier = Modifier.fillMaxWidth()
                 )
+                if(uiState.inValidTitle){
+                    Text(text = "Title could not be blank " ,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                }
                 
                 Spacer(modifier = Modifier.height(16.dp))
                 
                 // Description
                 OutlinedTextField(
-                    value = "",//TODO() ,
-                    onValueChange = { },//TODO() ,
+                    value = description,//TODO() ,
+                    onValueChange = { description = it},//TODO() ,
                     label = { Text("Description") },
+                    textStyle = TextStyle(textAlign = TextAlign.Right),
                     modifier = Modifier.fillMaxWidth(),
                     minLines = 3
                 )
+                if(uiState.inValidDesc){
+                    Text(text = "Description could not be blank " ,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                }
                 
                 Spacer(modifier = Modifier.height(16.dp))
                 
                 // Status dropdown
                 ExposedDropdownMenuBox(
-                    expanded = false ,//TODO
-                    onExpandedChange = { }, //TODO
+                    expanded = statusIsExpanded ,//TODO
+                    onExpandedChange = { statusIsExpanded = !statusIsExpanded }, //TODO
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     OutlinedTextField(
-                        value = "", //TODO
+                        value = status, //TODO
                         onValueChange = {},
                         readOnly = true,
                         label = { Text("Status") },
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = true, //TODO
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = statusIsExpanded, //TODO
                             ) },
+                        textStyle = TextStyle(),
                         modifier = Modifier
                             .fillMaxWidth()
                             .menuAnchor()
                     )
                     
                     ExposedDropdownMenu(
-                        expanded = false, //TODO
-                        onDismissRequest = { } ,//TODO
+                        expanded = statusIsExpanded, //TODO
+                        onDismissRequest = { statusIsExpanded = false} ,//TODO
                     ) {
                         listOf("Lost", "Found").forEach { option ->
                             DropdownMenuItem(
                                 text = { Text(text = option) },
                                 onClick = {
-                                    //TODO
+                                    status = option
+                                    statusIsExpanded = !statusIsExpanded
                                 }
                             )
                         }
@@ -139,36 +181,57 @@ fun AddItemScreen(
                 
                 // Category dropdown
                 ExposedDropdownMenuBox(
-                    expanded = false, //TODO
-                    onExpandedChange = { }, //TODO
+                    expanded = categoryIsExpanded, //TODO
+                    onExpandedChange = { categoryIsExpanded = !categoryIsExpanded}, //TODO
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     OutlinedTextField(
-                        value =  "Select Category",
+                        value =  categoryValue,
                         onValueChange = {},
                         readOnly = true,
                         label = { Text("Category") },
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = true) },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = categoryIsExpanded) },
                         modifier = Modifier
                             .fillMaxWidth()
                             .menuAnchor()
                     )
                     
                     ExposedDropdownMenu(
-                        expanded = false,//TODO
-                        onDismissRequest = { } , //TODO
+                        expanded = categoryIsExpanded,//TODO
+                        onDismissRequest = { categoryIsExpanded = false} , //TODO
                     ) {
                         categories.forEach { category ->
                             DropdownMenuItem(
                                 text = { Text(text = category.name) },
                                 onClick = {
                                     //TODO
+                                    categoryValue = category.name
+                                    categoryIsExpanded = false
                                 }
                             )
                         }
                     }
                 }
-                
+                if(uiState.inValidCategory){
+                    Text(text = "Please choose category " ,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                }
+                Spacer(modifier = Modifier.height(32.dp))
+                //Image url
+                OutlinedTextField(
+                    value = imageUrl,//TODO() ,
+                    onValueChange = {imageUrl = it },//TODO() ,
+                    label = { Text("Image url") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                if(uiState.inValidPicureUrl){
+                    Text(text = "Picture url could only be a url form " ,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                }
                 Spacer(modifier = Modifier.height(32.dp))
                 
                 // Submit button
@@ -176,14 +239,17 @@ fun AddItemScreen(
                     onClick = {
                         // In a real app, we would save the item here
                         // For now, just call the callback
+                        addItemViewModel.onEvent(AddItemEvent.SaveItem)
                         onItemAdded()
                     },
-                    enabled = true, // TODO
+                    enabled = uiState.isEnable, // TODO
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(text = "Add Item")
                 }
-            }
+                if(uiState.isLoading){
+                    Text(text = "Adding item...")
+                }            }
         }
     }
 }
--- app/src/main/java/com/example/loukatah/presentation/viewmodel/AddItemViewModel.kt
+++ app/src/main/java/com/example/loukatah/presentation/viewmodel/AddItemViewModel.kt
@@ -1,15 +1,128 @@
 package com.example.loukatah.presentation.viewmodel
 
+import androidx.compose.animation.fadeIn
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.loukatah.data.model.Item
 import com.example.loukatah.domain.usecase.AddItemUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.util.Date
 import javax.inject.Inject
 
 
 @HiltViewModel
 class AddItemViewModel @Inject constructor(private val addItemUseCase: AddItemUseCase): ViewModel() {
-
     //TODO: Add Item ViewModel logic
+    private val _uiState = MutableStateFlow(AddItemUiState())
+    val uiState : StateFlow<AddItemUiState> = _uiState.asStateFlow()
+
+    fun onEvent(event: AddItemEvent){
+        when(event) {
+            is AddItemEvent.TitleChange -> {
+                _uiState.update { it.copy(title = event.title) }
+                val isValid = validate(_uiState.value)
+                updateIsEnable(isValid);
+            }
+            is AddItemEvent.DescriptionChange -> {
+                _uiState.update { it.copy(description = event.description) }
+                val isValid = validate(_uiState.value)
+                updateIsEnable(isValid);
+            }
+            is AddItemEvent.StatusChange -> {
+                _uiState.update { it.copy(status = event.status) }
+            }
+            is AddItemEvent.PictureChange -> {
+                _uiState.update { it.copy(picture = event.picture) }
+                val isValid = validate(_uiState.value)
+                updateIsEnable(isValid);
+            }
+            is AddItemEvent.CategoryChange -> {
+                _uiState.update { it.copy(category = event.category) }
+                val isValid = validate(_uiState.value)
+                updateIsEnable(isValid);
+            }
+            is AddItemEvent.SaveItem -> {
+                saveItem();
+            }
+        }
+    }
+    fun saveItem() {
+        val currentState = _uiState.value
+        viewModelScope.launch {
+            addItemUseCase.invoke(Item(
+                    id = "9",
+                    title = currentState.title,
+                    description = currentState.description,
+                    status = currentState.status,
+                    item_category = currentState.category,
+                    coordinates = Pair(0.0, 0.0),
+                    date_lost = Date(),
+                    createdAt = "",
+                    updatedAt = Date(),
+                    picture = currentState.picture,
+                ))
+        }
+    }
+    fun validate(state: AddItemUiState): Boolean {
+        val isTitleAndDescriptionAndCategoryValid = isValidTitle(state.title) && isValidDescription(state.description) && isValidCategory(state.category)
+        val isPictureValid = isValidUrl(state.picture)
+        return isTitleAndDescriptionAndCategoryValid && isPictureValid
+
+    }
+    fun isValidTitle(title: String) : Boolean{
+        if(title.isNotBlank()){
+            _uiState.update { it.copy(inValidTitle = false) }
+            return true
+        }else{
+            _uiState.update { it.copy(inValidTitle = true) }
+            return  false
+        }
+    }
+    fun isValidDescription(description: String) : Boolean{
+        if(description.isNotBlank()){
+            _uiState.update { it.copy(inValidDesc = false) }
+            return true
+        }else{
+            _uiState.update { it.copy(inValidDesc = true) }
+            return  false
+        }
+    }
+
+    fun isValidUrl(url: String): Boolean {
+        // Regular expression to match a URL
+        val urlRegex = """^(https?://)?([\w.-]+)(\.[a-zA-Z]{2,})(:[0-9]{1,5})?(/.*)?${'$'}""".toRegex()
+        if(url.isNotBlank() && urlRegex.matches(url)){
+            _uiState.update { it.copy(inValidPicureUrl = false) }
+            return true
+        }else{
+            _uiState.update { it.copy(inValidPicureUrl = true) }
+            return  false
+        }
+    }
+    fun isValidCategory(category: String) : Boolean{
+        if(category.isNotBlank()){
+            _uiState.update { it.copy(inValidCategory = false) }
+            return true
+        }else{
+            _uiState.update { it.copy(inValidCategory = true) }
+            return  false
+        }
+    }
+
+    fun updateIsEnable(isValid : Boolean){
+        if(isValid){
+            _uiState.update { it.copy(isEnable = true) }
+        }else{
+            _uiState.update { it.copy(isEnable = false) }
+        }
+    }
 
 }
 
@@ -18,8 +131,13 @@ data class AddItemUiState(
     val description: String = "",
     val status: String = "",
     val picture: String = "",
-    val category: String = ""
-
+    val category: String = "",
+    val isEnable: Boolean = false,
+    val isLoading: Boolean = false,
+    val inValidTitle : Boolean = true,
+    val inValidDesc : Boolean = true,
+    val inValidPicureUrl : Boolean = true,
+    val inValidCategory: Boolean = true
 )
 
 sealed class AddItemEvent {
